fe5c0163ba6bdaf6489117e7b71ee1d1c60f48b6
Berat Uslu <berat.uslu.me@gmail.com>
2019-09-06 00:36:42 +0300

    web-push-subscribe credentials saved to the db

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DashApp from './dashApp';
-import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import DashApp from "./dashApp";
+import * as serviceWorker from "./serviceWorker";
+import { subscribeUser } from "./subscription";
+import "antd/dist/antd.css";
 
-ReactDOM.render(<DashApp />, document.getElementById('root'));
+ReactDOM.render(<DashApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
+subscribeUser();
