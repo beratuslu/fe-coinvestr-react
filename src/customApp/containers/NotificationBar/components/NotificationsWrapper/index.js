9ac70fda656d8a3402487aa990e6e7944e5f860a
Berat Uslu <berat.uslu.me@gmail.com>
2020-08-09 21:36:05 +0300

    .

--- src/customApp/containers/NotificationBar/components/NotificationsWrapper/index.js
+++ src/customApp/containers/NotificationBar/components/NotificationsWrapper/index.js
@@ -10,6 +10,7 @@ class Notifications extends Component {
   constructor(props) {
     super(props);
     this.handleVisibleChange = this.handleVisibleChange.bind(this);
+    this.handleMarkAsRead = this.handleMarkAsRead.bind(this);
     this.hide = this.hide.bind(this);
     this.state = {
       visible: false,
@@ -21,36 +22,52 @@ class Notifications extends Component {
   handleVisibleChange() {
     this.setState({ visible: !this.state.visible });
   }
-  handleMarkAsRead() {
-    this.setState({ visible: !this.state.visible });
-  }
-  handleMarkAsRead() {
-    console.log("ssss");
+  // handleMarkAsRead() {
+  //   this.setState({ visible: !this.state.visible });
+  // }
+  handleMarkAsRead(notifIdArr) {
+    console.log("Notifications ", notifIdArr);
   }
   renderItems() {
     const { customizedTheme, enums } = this.props;
     const { notifications } = this.props.notifications;
     const notifEnums = enums.enumsAndConstants.notifications;
     let count = 0;
+    let notifIdArr = [];
     return notifications.map((notification, index) => {
-      let devider = null;
-      if (count === 5) {
-        count = 0;
-        devider = (
-          <a className="devider" onClick={this.handleMarkAsRead} href="#">
-            mark below 5 as read
-          </a>
-        );
-      }
+      notifIdArr.push(notification.id);
+      // let devider = false;
+      // if (count === 5) {
+      //   count = 0;
+      //   devider = (
+      //     <a
+      //       key={"aSs" + notification.id}
+      //       className="devider"
+      //       onClick={() => {
+      //         console.log(
+      //           "Notifications -> renderItems -> notifIdArr",
+      //           notifIdArr
+      //         );
+      //         return this.handleMarkAsRead(notifIdArr);
+      //       }}
+      //     >
+      //       mark below 5 as read {notifIdArr.toString()}
+      //     </a>
+      //   );
+      //   notifIdArr = [];
+      // }
       count++;
       return (
-        <div className="notifWrapper" key={notification.id}>
-          {devider}
+        <div className="notifWrapper" key={"asSs" + notification.id}>
+          {/* {devider} */}
           <a className="isoDropdownListItem" href="# ">
             <h5>{notifEnums[notification.notifType].title}</h5>
             <p>{notifEnums[notification.notifType].body}</p>
             <Tooltip placement="left" title="mark as read">
-              <span className="isRead"></span>
+              <span
+                className="isRead"
+                onClick={() => this.handleMarkAsRead([notification.id])}
+              ></span>
             </Tooltip>
           </a>
         </div>
