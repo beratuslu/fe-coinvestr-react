a00fe69568932d0300d510d4ab01acd86bc9049d
Berat Uslu <berat.uslu.me@gmail.com>
2020-08-21 00:18:49 +0300

    .

---
+++ src/customApp/containers/Firebase/components/NotifMessages/CopyTradeActivityProfitSellOrderFilledPartially/index.js
@@ -0,0 +1,67 @@
+// import React, { useState, useEffect, useCallback } from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import MessageWrapper from "../styles";
+import actions from "../../../_redux/actions";
+import { Provider } from "react-redux";
+import { store, history } from "../../../../../../redux/store";
+import { Link, Router, BrowserRouter, withRouter } from "react-router-dom";
+
+class CopyTradeStart extends Component {
+  constructor(props) {
+    super(props);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+
+    this.state = {
+      //   active: "post",
+      //   visible: false,
+    };
+  }
+
+  componentDidMount() {}
+
+  onMouseEnter(e) {
+    const { notification, markNotificationsAsRead } = this.props;
+    markNotificationsAsRead([notification.id]);
+  }
+  render() {
+    const { notification, enumsAndConstants, app, bar } = this.props;
+    const { params } = notification;
+    const { lastExecutionQty, buyPrice, symbol, copyTradeId } = params;
+
+    return (
+      <MessageWrapper onMouseEnter={this.onMouseEnter}>
+        <div>
+          Profit sell order filled partially of{" "}
+          <Link to={`/dashboard/copy-trade/${copyTradeId}`}>copy trade</Link>.
+          {(app.view === "DesktopView" || bar) && (
+            <div>
+              <br />
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Quantity:</td>
+                    <td>
+                      <strong>{lastExecutionQty}</strong>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </MessageWrapper>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    profile: state.Profile,
+    auth: state.Auth,
+    enumsAndConstants: state.enumsAndConstants.enumsAndConstants,
+    app: state.App,
+  };
+}
+export default connect(mapStateToProps, { ...actions })(CopyTradeStart);
+// export default CopyTradeStart;
--- src/customApp/containers/Firebase/components/NotifMessages/index.js
+++ src/customApp/containers/Firebase/components/NotifMessages/index.js
@@ -2,10 +2,16 @@ import InsufficientBalanceForSocketCheck from "./InsufficientBalanceForSocketChe
 import CopyTradeStart from "./CopyTradeStart";
 import CopyTradeFailedDueToInsufficientBalance from "./CopyTradeFailedDueToInsufficientBalance";
 import CopyTradeActivityBuyOrderPlaced from "./CopyTradeActivityBuyOrderPlaced";
+import CopyTradeActivityBuyOrderFilledPartially from "./CopyTradeActivityBuyOrderFilledPartially";
+import CopyTradeActivityBuyOrderFilled from "./CopyTradeActivityBuyOrderFilled";
+import CopyTradeActivityProfitSellOrderFilledPartially from "./CopyTradeActivityProfitSellOrderFilledPartially";
 
 export default {
   INSUFFICIENT_BALANCE_FOR_SOCKET_CHECK: InsufficientBalanceForSocketCheck,
   COPY_TRADE_START: CopyTradeStart,
   COPY_TRADE_FAILED_DUE_TO_INSUFFICIENT_BALANCE: CopyTradeFailedDueToInsufficientBalance,
   COPY_TRADE_ACTIVITY_BUY_ORDER_PLACED: CopyTradeActivityBuyOrderPlaced,
+  COPY_TRADE_ACTIVITY_BUY_ORDER_FILLED_PARTIALLY: CopyTradeActivityBuyOrderFilledPartially,
+  COPY_TRADE_ACTIVITY_BUY_ORDER_FILLED: CopyTradeActivityBuyOrderFilled,
+  COPY_TRADE_ACTIVITY_PROFIT_SELL_ORDER_FILLED_PARTIALLY: CopyTradeActivityProfitSellOrderFilledPartially,
 };
