3fa5b5521a67677d059d985b5c9999dd45ee6334
Berat Uslu <berat.uslu.me@gmail.com>
2020-09-12 21:14:34 +0300

    .

--- src/customApp/containers/Auth/_redux/saga.js
+++ src/customApp/containers/Auth/_redux/saga.js
@@ -40,7 +40,11 @@ export function* loginSuccess() {
     yield (axios.defaults.headers.common = {
       Authorization: `Bearer ${payload.token}`,
     });
-    yield put(push("/dashboard/profile/asdsss"));
+    yield put(
+      push(
+        "/dashboard/profile/asdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsssasdsss"
+      )
+    );
   });
 }
 
