94751b4dff329960509d993c290a4f541a260984
Berat Uslu <berat.uslu.me@gmail.com>
2020-12-27 03:09:35 +0300

    .

---
+++ build/service-worker.js
@@ -0,0 +1,42 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dashboard/precache-manifest.fdb12a5d8499e3044ce896db0709dd54.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dashboard/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
+self.addEventListener("push", event => {
+  const data = event.data.json();
+  console.log("New notification", data);
+  const options = {
+    body: data.body
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
